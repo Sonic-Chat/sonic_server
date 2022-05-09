67d66643fb7db99c5fbf57618ee36460eb8504ad
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-05-09 10:27:26 +0530

    MikroORM and Database Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,19 @@
+import { Dummy } from './models/dummy.model';
 import { Module } from '@nestjs/common';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+
 @Module({
-  imports: [],
+  imports: [
+    MikroOrmModule.forRoot({
+      dbName: process.env.DATABASE_NAME,
+      name: 'sonic-database',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      type: 'postgresql',
+      entities: [Dummy],
+    }),
+  ],
 })
 export class AppModule {}
