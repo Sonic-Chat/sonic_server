d607b6ae6516b85f9a3dbb22628ef05c16666769
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-05-11 17:59:25 +0530

    Added ValidfationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { PrismaService } from './modules/prisma/prisma.service';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
   await prismaService.enableShutdownHooks(app);
 
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT || 5000);
 }
