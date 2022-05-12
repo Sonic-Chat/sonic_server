f9a12f6f0e8f0bbeb2756fc1e29c6197fd2b13d8
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-05-12 10:14:56 +0530

    Auth Guard Comments

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -8,6 +8,9 @@ import {
 } from '@nestjs/common';
 import { CredentialsService } from 'src/modules/credentials/credentials.service';
 
+/**
+ * Guard Service Implementation for Firebase account authentication.
+ */
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(
@@ -15,33 +18,50 @@ export class AuthGuard implements CanActivate {
     private readonly credentialsService: CredentialsService,
   ) {}
 
-  private async authenticateRequest(request): Promise<boolean> {
-    try {
-      if (
-        request.headers.authorization &&
-        request.headers.authorization.split(' ')[0] === 'Bearer'
-      ) {
-        const authToken = request.headers.authorization.split(' ')[1];
+  /**
+   * Method Implementation for authenticating requests using Firebase.
+   * @param request HTTP Request Object.
+   * @returns boolean if the request is authenticated or not.
+   */
+  private async authenticateRequest(request: any): Promise<boolean> {
+    // Check if authentication header is present and valid.
+    if (
+      request.headers.authorization &&
+      request.headers.authorization.split(' ')[0] === 'Bearer'
+    ) {
+      // Extract token from the header.
+      const authToken = request.headers.authorization.split(' ')[1];
 
+      try {
+        // Authenticate token using firebase.
         const firebaseUser =
           await this.firebaseService.firebaseAuth.verifyIdToken(authToken);
+
+        // Set user header on the request object.
         request.user = await this.credentialsService.getCredential({
           firebaseId: firebaseUser.uid,
         });
+
+        // Return true for authenticated.
         return true;
-      } else {
+      } catch (error) {
+        console.log(error);
         throw new ForbiddenException({
           message: AuthError.UNAUTHENTICATED,
         });
       }
-    } catch (error) {
-      console.log(error);
+    } else {
       throw new ForbiddenException({
         message: AuthError.UNAUTHENTICATED,
       });
     }
   }
 
+  /**
+   * Method Implementation for Authentication Guard.
+   * @param context NestJS Execution Context.
+   * @returns boolean if the request is valid or not.
+   */
   async canActivate(context: ExecutionContext): Promise<boolean> {
     return await this.authenticateRequest(context.switchToHttp().getRequest());
   }
