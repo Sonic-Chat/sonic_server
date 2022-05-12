c18d4f4140f54043f29190e4e8745e4966617e5b
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-05-12 10:15:03 +0530

    Account Service Comments

--- src/modules/account/account.service.ts
+++ src/modules/account/account.service.ts
@@ -2,21 +2,24 @@ import { PrismaService } from './../prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 import { Account, Prisma } from '@prisma/client';
 
+/**
+ * Service Implementation for Account Module.
+ */
 @Injectable()
 export class AccountService {
   constructor(private readonly prismaService: PrismaService) {}
 
+  /**
+   * Service implementation for fetching a user.
+   * @param where Query parameters to fetch a user.
+   * @returns Account object or null.
+   */
   public async getUser(
-    accountWhereUniqueInput: Prisma.AccountWhereUniqueInput,
+    where: Prisma.AccountWhereUniqueInput,
   ): Promise<Account | null> {
+    // Pass the query parameters to find a unique account and return it.
     return await this.prismaService.account.findUnique({
-      where: accountWhereUniqueInput,
+      where,
     });
-  }
-
-  public async createUser(
-    newUser: Prisma.AccountCreateInput,
-  ): Promise<Account> {
-    return await this.prismaService.account.create({ data: newUser });
   }
 }
