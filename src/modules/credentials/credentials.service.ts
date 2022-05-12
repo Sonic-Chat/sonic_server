a285653dd9188d063f5e69cb6fed6631dbadd956
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-05-12 10:15:29 +0530

    Credentials Service comments

--- src/modules/credentials/credentials.service.ts
+++ src/modules/credentials/credentials.service.ts
@@ -2,30 +2,56 @@ import { PrismaService } from './../prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 import { Credentials, Prisma } from '@prisma/client';
 
+/**
+ * Service Implementation for Credentials Module.
+ */
 @Injectable()
 export class CredentialsService {
   constructor(private readonly prismaService: PrismaService) {}
 
+  /**
+   * Service Implementation for fetching credentials.
+   * @param where Query Parameters to fetch corresponding Credentials Object.
+   * @returns Credentials object or null.
+   */
   public async getCredential(
-    credentialsWhereUniqueInput: Prisma.CredentialsWhereUniqueInput,
+    where: Prisma.CredentialsWhereUniqueInput,
   ): Promise<Credentials | null> {
+    // Pass the parameters and return the result.
     return await this.prismaService.credentials.findUnique({
-      where: credentialsWhereUniqueInput,
+      where,
       include: {
         account: true,
       },
     });
   }
 
+  /**
+   * Service Implementation for creation of credentials.
+   * @param data Parameters to create Credentials object.
+   * @returns Credentials Object.
+   */
   public async createCredential(
     data: Prisma.CredentialsCreateInput,
   ): Promise<Credentials> {
+    // Pass the parameters and return the result.
     return await this.prismaService.credentials.create({ data });
   }
 
+  /**
+   * Service Implementation for deleting credentials.
+   * @param where Query Parameters to delete corresponding Credentials Object.
+   * @returns Credentials object.
+   */
   public async deleteCredential(
     where: Prisma.CredentialsWhereUniqueInput,
   ): Promise<Credentials> {
-    return await this.prismaService.credentials.delete({ where });
+    // Pass the parameters and return the result.
+    return await this.prismaService.credentials.delete({
+      where,
+      include: {
+        account: true,
+      },
+    });
   }
 }
