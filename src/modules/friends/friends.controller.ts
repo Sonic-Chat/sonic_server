797f2aca397fbf49d556eb6d71bea18ab25c9805
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-05-13 22:19:06 +0530

    Controller Implementation for updating a friend request. Closes #28

--- src/modules/friends/friends.controller.ts
+++ src/modules/friends/friends.controller.ts
@@ -1,9 +1,10 @@
 import { CreateRequestDto } from './../../dto/friends/create-request.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Put, UseGuards } from '@nestjs/common';
 import { FriendsService } from './friends.service';
 import { User } from 'src/decorators/user.decorator';
 import { Credentials, Friends } from '@prisma/client';
+import { UpdateRequestDto } from 'src/dto/friends/update-request.dto';
 
 /**
  * Controller Implementation for Friend Requests Module.
@@ -25,5 +26,27 @@ export class FriendsController {
     @Body() createRequestDto: CreateRequestDto,
   ): Promise<Friends> {
     return await this.friendsService.createRequest(user, createRequestDto);
+  }
+
+  /**
+   * Controller Implementation for updating a friend request.
+   * @param user Logged In User Details.
+   * @param updateRequestDto DTO Object for Updating Friend Request.
+   * @returns Updated Friend Request.
+   */
+  @Put()
+  @UseGuards(AuthGuard)
+  public async updateRequest(
+    @User() user: Credentials,
+    @Body() updateRequestDto: UpdateRequestDto,
+  ): Promise<Friends> {
+    return await this.friendsService.updateRequest(user, {
+      where: {
+        id: updateRequestDto.id,
+      },
+      data: {
+        status: updateRequestDto.status,
+      },
+    });
   }
 }
