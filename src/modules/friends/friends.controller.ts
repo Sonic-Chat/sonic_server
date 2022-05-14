096548c3daa6ce60ad3789a132714fae325ab92c
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-05-14 11:08:13 +0530

    Controller Implementation for fetching a friend request. Closes #33

--- src/modules/friends/friends.controller.ts
+++ src/modules/friends/friends.controller.ts
@@ -1,18 +1,87 @@
 import { CreateRequestDto } from './../../dto/friends/create-request.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
-import { Body, Controller, Delete, Post, Put, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { FriendsService } from './friends.service';
 import { User } from 'src/decorators/user.decorator';
 import { Credentials, Friends } from '@prisma/client';
 import { UpdateRequestDto } from 'src/dto/friends/update-request.dto';
 import { DeleteRequestDto } from 'src/dto/friends/delete-request.dto';
+import { AccountService } from '../account/account.service';
+import { fetchRequestDto } from 'src/dto/friends/fetch-request.dto';
+import { FriendsError } from 'src/enum/error-codes/friends/friends-error.enum';
 
 /**
  * Controller Implementation for Friend Requests Module.
  */
 @Controller('v1/friends')
 export class FriendsController {
-  constructor(private readonly friendsService: FriendsService) {}
+  constructor(
+    private readonly friendsService: FriendsService,
+    private readonly accountService: AccountService,
+  ) {}
+
+  /**
+   * Controller Implementation for fetching a friend request.
+   * @param user Logged In User Details
+   * @param fetchRequestDto DTO Object to Fetch Friend Request
+   * @returns Friend Request Object or error.
+   */
+  @Get('account')
+  @UseGuards(AuthGuard)
+  public async getFriendRequest(
+    @User() user: Credentials,
+    @Body() fetchRequestDto: fetchRequestDto,
+  ): Promise<Friends | null> {
+    // Fetching logged in user account details.
+    const account = await this.accountService.getUser({
+      credentialsId: user.id,
+    });
+
+    // Fetching request from database.
+    const checkRequest = await this.friendsService.getFriendRequest({
+      where: {
+        AND: [
+          {
+            accounts: {
+              some: {
+                id: account.id,
+              },
+            },
+          },
+          {
+            accounts: {
+              some: {
+                id: fetchRequestDto.accountId,
+              },
+            },
+          },
+        ],
+      },
+      include: {
+        accounts: true,
+      },
+    });
+
+    // If request does not exists, throw an HTTP exception.
+    if (!checkRequest) {
+      throw new NotFoundException({
+        message: FriendsError.REQUEST_NOT_FOUND,
+      });
+    }
+    // Else return the result.
+    else {
+      return checkRequest;
+    }
+  }
 
   /**
    * Controller Implementation for creating a friend request.
