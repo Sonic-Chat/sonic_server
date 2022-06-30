ba999574621a09a97b99ad1c77159917e906f02e
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-06-30 15:47:29 +0530

    Controller Implementation for updating group chat. Closes #110

--- src/modules/chat/chat.controller.ts
+++ src/modules/chat/chat.controller.ts
@@ -1,9 +1,10 @@
 import { CreateGroupChatDto } from './../../dto/chat/create-group-chat.dto';
 import { ChatService } from './chat.service';
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Put, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { User } from 'src/decorators/user.decorator';
 import { Chat, Credentials } from '@prisma/client';
+import { UpdateGroupChatDto } from 'src/dto/chat/update-group-chat.dto';
 
 /**
  * Controller Implementation for Chat Module.
@@ -27,6 +28,23 @@ export class ChatController {
     return await this.chatService.createGroupChatService(
       user,
       createGroupChatDto,
+    );
+  }
+
+  /**
+   * Controller Implementation for updating group chat.
+   * @param user Logged In User.
+   * @param updateGroupChatDto DTO Implementation for updating group chat.
+   * @returns Updated Chat.
+   */
+  @Put('group')
+  public async updateGroupChatService(
+    @User() user: Credentials,
+    @Body() updateGroupChatDto: UpdateGroupChatDto,
+  ): Promise<Chat> {
+    return await this.chatService.updateGroupChatService(
+      user,
+      updateGroupChatDto,
     );
   }
 }
